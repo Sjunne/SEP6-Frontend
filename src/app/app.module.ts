7fada09059bf3360faa7eeeb8b1e08bd1584cdbc
Jens Mattias Leth Sjunnesson <mlsj@netcompany.com>
2022-04-30 16:00:52 +0200

    Adding toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolBarComponent } from './components/tool-bar/tool-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolBarComponent
+    ToolBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MdbFormsModule,
+    MdbRippleModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
