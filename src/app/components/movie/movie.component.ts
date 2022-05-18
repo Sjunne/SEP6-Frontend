e240f08bc666715ffb3f8f4da5c63b0a97ae9ee3
MariaCFFrandsen <56017845+MariaCFFrandsen@users.noreply.github.com>
2022-05-18 11:17:14 +0200

    from movie to person

--- src/app/components/movie/movie.component.ts
+++ src/app/components/movie/movie.component.ts
@@ -3,8 +3,10 @@ import {MovieService} from "../../services/movie.service";
 import {HttpClient, HttpXhrBackend} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Movie} from "../../models/Movie";
-import { ActivatedRoute } from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import { FullMovie } from '../../models/FullMovie';
+import {ActorsService} from "../../services/actors.service";
+import {ChartReadyEvent} from "ng2-google-charts";
 
 
 @Component({
@@ -15,7 +17,12 @@ import { FullMovie } from '../../models/FullMovie';
 export class MovieComponent implements OnInit {
 
   public movie! :FullMovie;
-  constructor(private service: MovieService, private activated: ActivatedRoute ) {
+  private searchString!: string;
+  public id!: number;
+  constructor(private service: MovieService,
+              private activated: ActivatedRoute,
+              private personService: ActorsService,
+              private router: Router) {
   }
   ngOnInit(): void {
     this.activated.paramMap.subscribe(map => {
@@ -23,6 +30,19 @@ export class MovieComponent implements OnInit {
       this.service.getFullMovie(movieId).subscribe(movie => {
         this.movie = movie;
       });
+
     });
+
   }
+
+  public SelectPerson(name: string) {
+    this.personService.getPeople(name).subscribe(actorList => {
+      this.id = actorList[0].id;
+      console.log(this.id)
+      this.router.navigate(['Person', this.id]);
+    });
+
+
+  }
+
 }
